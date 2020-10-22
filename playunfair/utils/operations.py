a691f2e447e269803bfae4bbb7c4048a8595733c
AidilRiski <riskisultan@yahoo.com>
2020-10-22 20:19:36 +0700

    Add permutation operation.

--- playunfair/utils/operations.py
+++ playunfair/utils/operations.py
@@ -3,3 +3,22 @@ import numpy
 def xorBlock(blockA, blockB):
     result = numpy.bitwise_xor(blockA, blockB)
     return result.tolist()
+
+def permutate(permutationMatrix, block):
+    permutationBits = []
+    for row in permutationMatrix:
+        permutationBitRow = []
+        for bitN in row:
+            bitMask = createMask(bitN % 8)
+            byteIndex = byteOfBitN(bitN)
+            operationResult = bitMask & block[byteIndex]
+            operationResult = 1 if operationResult else 0
+            permutationBitRow.append(operationResult)
+        permutationBits.append(permutationBitRow)
+    return permutationBits
+
+def createMask(bitN):
+    return 1 << bitN
+
+def byteOfBitN(bitN):
+    return bitN // 8
