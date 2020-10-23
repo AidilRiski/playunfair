63bdc07e001ba12a2f3027290cd0c94b8ebd0367
AidilRiski <riskisultan@yahoo.com>
2020-10-23 11:55:08 +0700

    Add interactions.

--- playunfair/__main__.py
+++ playunfair/__main__.py
@@ -2,74 +2,62 @@ from playunfair.utils import files, operations
 from playunfair.algorithm import cbc, ctr, ecb, easyEncrypt, feistel, playunfair, sbox
 from playunfair.constants import feistel as feistelConstants
 from playunfair.constants import sbox as sboxConstants
+from playunfair.constants import cbc as cbcConstants
+import argparse
 
-if __name__ == '__main__':
-    print("CBC")
-    filename = '.gitignore'
+def main(args):
+    key = args.key
+    filename = args.filename
+    method = args.method
+    encrypt = args.encrypt
+    decrypt = args.decrypt
+
     data = files.openFile(filename)
     blocks = files.splitToBlocks(8, data)
-    key = [255, 255, 255, 1, 13, 69, 210, 155]
-    initialVector = [0, 0, 0, 0, 0, 0, 0, 0]
-    cipher = cbc.encrypt(key, initialVector, blocks, playunfair.networkEncrypt)
-    newData = files.joinBlocks(cipher)
-    files.writeFile('cipher.' + filename, bytearray(newData))
-    data = files.openFile('cipher.' + filename)
-    blocks = files.splitToBlocks(8, data)
-    plainBlocks = cbc.decrypt(key, initialVector, cipher, playunfair.networkDecrypt)
-    plainData = files.joinBlocks(plainBlocks)
-    files.writeFile('decipher.' + filename, bytearray(plainData))
+    key = bytearray(key, 'utf-8')
 
-    # print("ECB")
-    # data = files.openFile(".gitignore")
-    # print([el for el in data[:12]])
-    # blocks = files.splitToBlocks(3, data)
-    # key = [255, 255, 255]
-    # cipher = ecb.encrypt(key, blocks, easyEncrypt.encrypt)
-    # newData = files.joinBlocks(cipher)
-    # print(newData[:12])
-    # plainBlocks = ecb.decrypt(key, cipher, easyEncrypt.encrypt)
-    # plainData = files.joinBlocks(plainBlocks)
-    # print(plainData[:12])
+    if encrypt:
+        cipher = []
+        if method == 'cbc':
+            cipher = cbc.encrypt(key, cbcConstants.INITIAL_VECTOR, blocks, playunfair.networkEncrypt)
+        elif method == 'ecb':
+            cipher = ecb.encrypt(key, blocks, playunfair.networkEncrypt)
+        elif method == 'ctr':
+            cipher = ctr.encrypt(key, blocks, playunfair.networkEncrypt)
+        newData = files.joinBlocks(cipher)
+        files.writeFile(filename + '.cipher', bytearray(newData))
+    elif decrypt:
+        plain = []
+        if method == 'cbc':
+            plain = cbc.decrypt(key, cbcConstants.INITIAL_VECTOR, blocks, playunfair.networkEncrypt)
+        elif method == 'ecb':
+            plain = ecb.decrypt(key, blocks, playunfair.networkEncrypt)
+        elif method == 'ctr':
+            plain = ctr.decrypt(key, blocks, playunfair.networkEncrypt)
+        newData = files.joinBlocks(plain)
+        files.writeFile(filename + '.plain', bytearray(newData))
 
-    # print("CTR")
-    # data = files.openFile(".gitignore")
-    # print([el for el in data[:12]])
-    # blocks = files.splitToBlocks(3, data)
-    # key = [255, 255, 255]
-    # cipher = ctr.encrypt(key, blocks, easyEncrypt.encrypt)
-    # newData = files.joinBlocks(cipher)
-    # print(newData[:12])
-    # plainBlocks = ctr.decrypt(key, cipher, easyEncrypt.encrypt)
-    # plainData = files.joinBlocks(plainBlocks)
-    # print(plainData[:12])
+def checkArgs(args):
+    if args.encrypt and args.decrypt:
+        raise Exception('cannot encrypt and decrypt at the same time')
+    elif not args.encrypt and not args.decrypt:
+        raise Exception('specify whether you are decrypting or encrypting')
 
-    # print("FSTL")
-    # data = files.openFile(".gitignore")
-    # print([el for el in data[:12]])
-    # blocks = files.splitToBlocks(8, data)
-    # key = [0, 0, 0, 0, 255, 0, 255, 0]
-    # cipher = feistel.encrypt(key, blocks, playunfair.encryptFeistel)
-    # newData = files.joinBlocks(cipher)
-    # print(newData[:12])
-    # plainBlocks = feistel.decrypt(key, cipher, playunfair.encryptFeistel)
-    # plainData = files.joinBlocks(plainBlocks)
-    # print(plainData[:12])
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="PlayUnfair Cipher"
+    )
 
-    # print("PLFR")
-    # data = files.openFile(".gitignore")
-    # print([el for el in data[:8]])
-    # blocks = files.splitToBlocks(8, data)
-    # table = playunfair.createTable([3, 2, 45, 11, 2, 3])
-    # cipher = playunfair.encryptPlayfair(table, blocks[0])
-    # print(cipher)
-    # plainBlock = playunfair.decryptPlayfair(table, cipher)
-    # print(plainBlock)
+    parser.add_argument('method', choices=['cbc', 'ecb', 'ctr'], help='specify the method to use', action='store')
+    parser.add_argument('key', help='specify the key to use', action='store')
+    parser.add_argument('filename', help='specify the filename', action='store')
+    parser.add_argument('-e', '--encrypt', help='encrypt the file using the keyword', action='store_true')
+    parser.add_argument('-d', '--decrypt', help='decrypt the file using the keyword', action='store_true')
 
-    # print("SBOX")
-    # data = files.openFile(".gitignore")
-    # print([el for el in data[:8]])
-    # blocks = files.splitToBlocks(8, data)
-    # cipher = sbox.encrypt(sboxConstants.AES_MATRIX, blocks[0])
-    # print(cipher)
-    # plainBlock = sbox.decrypt(sboxConstants.AES_MATRIX, cipher)
-    # print(plainBlock)
+    args = parser.parse_args()
+
+    try:
+        checkArgs(args)
+        main(args)
+    except Exception as e:
+        parser.error(e)
